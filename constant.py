d1552635ec10a4f3585751df011880e268967883
Kamil_Herbetko <herbetko.kamil@gmail.com>
2021-12-08 23:03:21 +0100

    Adding a constant file with gpus to search for.

---
+++ constant.py
@@ -0,0 +1,19 @@
+LIST_OF_NVIDIA_GPUS = [
+    "rtx 3090",
+    "rtx 3080 ti",
+    "rtx 3080",
+    "rtx 3070 ti",
+    "rtx 3070",
+    "rtx 3060 ti",
+    "rtx 3060"
+]
+
+LIST_OF_AMD_GPUS = [
+    "rx 6900 xt",
+    "rx 6800 xt",
+    "rx 6800",
+    "rx 6700 xt",
+    "rx 6700",
+    "rx 6600 xt",
+    "rx 6600"
+]
